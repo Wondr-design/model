5ad340755ba2a0ff5bf31d0b9c5a4dc540482856
Wondr <wondr.design@gmail.com>
2025-05-07 11:52:24 +0100

    v1.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-// import Text from "./components/Text";
+import Text from "./components/Text";
 import GFunctions from "./components/GFunctions";
 
 const App = () => {
   return (
     <>
-      {/* <Text /> */}
+      <Text />
       <GFunctions />
     </>
   );
